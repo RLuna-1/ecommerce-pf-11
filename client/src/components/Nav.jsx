bff0514a02d2649eede93ae2ba11778cf5a26634
TriNec13 <ivan.nicolas.valero.11@gmail.com>
2023-05-30 23:10:17 -0300

    Correcion de importaciones
    index y app

---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {Link, useLocation} from "react-router-dom"
+import styles from "../css/Nav.module.css"
+
+const Nav = (props) => {
+    const {pathname} = useLocation();
+    return (
+        <div> /* DIV GENERAL */
+            <Link to="/home"> <button className={styles.ButtonNav}>HOME</button> </Link>
+            <Link to="/allcards"> <button className={styles.ButtonNav}>All Cards</button> </Link>
+            <Link to="/favorites"> <button className={styles.ButtonNav}>FAVORITES</button> </Link>
+            <Link to="/about"> <button className={styles.ButtonNav}>ABOUT</button> </Link>
+            {pathname === "/home" && <SearchBar onSearch={props.onSearch}/>}
+        </div>
+    )
+}
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './css/index';
+import './css/index.css';
 import App from './view/App';
 import reportWebVitals from './reportWebVitals';
 
