c89abe6eeb3aea1051be078cfd6db7dc8c1a5813
Juan Gomez <juancarlos.gm931@gmail.com>
2023-05-31 09:16:05 -0500

    client/src/index.js
    Se agrega función de cambio en el titulo de la pestaña.

--- client/src/index.js
+++ client/src/index.js
@@ -11,6 +11,17 @@ root.render(
   </React.StrictMode>
 );
 
+//cambia el nombre de la pestaña
+let previousTitle = document.title;
+window.addEventListener('blur', () => {
+	previousTitle = document.title;
+	document.title = 'No te vayas!! Vuelve';
+});
+
+//devuelve el titulo original
+window.addEventListener('focus', () => {
+	document.title = previousTitle;
+});
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
