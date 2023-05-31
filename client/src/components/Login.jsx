d9a105f10587f2155476956fa09172f95eabf03e
lucas-357 <lucas.rojo.180@gmail.com>
2023-05-31 20:22:57 -0300

    login y about

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,108 +1,52 @@
-import "../css/Login.css";
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
+const Login = () => {
+  const initialValues = {
+    email: "",
+    password: ""
+  };
 
-export default function UserLogin() {
-    const dispatch = useDispatch();
-    const classes = useStyles();
+  const validationSchema = Yup.object().shape({
+    email: Yup.string().email("Ingrese un email válido").required("Ingrese su email"),
+    password: Yup.string().required("Ingrese su contraseña")
+  });
 
-    const [state, setState] = useState({
-        email: "",
-        password: "",
-    })
-    const [errors, setErrors] = useState({});
+  const handleSubmit = (values, { setSubmitting }) => {
+    // Aquí puedes realizar la lógica de inicio de sesión, como enviar una solicitud al servidor, validar las credenciales, etc.
+    console.log(values);
+    setSubmitting(false);
+  };
 
-
-    const handleInputChange = (e) => {
-        setState({
-            ...state,
-            [e.target.name]: e.target.value
-        })
-
-        console.log(state)
-
-    }
-    
-    const validate = (state) => {
-        let errors = {};
-        if (!state.email) {
-            errors.email = 'Por favor, introduzca un email';
-        } else if (!state.email.includes("@")) {
-            errors.email = 'Por favor, introduzca un formato de email valido';
-        }
-
-        if (!state.password) {
-            errors.password = 'Por favor, introduzca una contraseña';
-
-        }
-        return errors;
-
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setErrors(validate({
-            ...state,
-            [e.target.name]: e.target.value
-        }));
-        console.log(e)
-        if (state.email && state.password) {
-            console.log(state)
-            dispatch(loginUser(state))
-        }
-    }
   return (
-    <form onSubmit={handleSubmit} >
-        <Grid container component="main" className={classes.root}>
-            <CssBaseline />
-            <Grid item xs={false} sm={4} md={7} className={classes.image} />
-            <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-                <div className={classes.paper}>
-                    <Typography style={{ color: "black" }} component="h1" variant="h5">
-                        Accedé a tu cuenta
-                    </Typography>
-                    <form className={classes.form} noValidate>
-                        <TextField
-                            onChange={handleInputChange}
-                            variant="outlined"
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="email"
-                            label="Dirección de email"
-                            name="email"
-                            autoComplete="email"
-                            autoFocus
-                        />
-                        {errors.email && (
-                            <p style={{ color: "red" }}>{errors.email}</p>
-                        )}
-                        <TextField
-                            onChange={handleInputChange}
-                            variant="outlined"
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Contraseña"
-                            type="password"
-                            id="password"
-                            autoComplete="current-password"
-                        />
-                        {errors.password && (
-                            <p style={{ color: "red" }}>{errors.password}</p>
-                        )}
-                        <ColorButton
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            className={classes.submit}
-                        >
-                            INGRESAR
-                        </ColorButton>
-                    </form>
-                </div>
-            </Grid>
-        </Grid>
-    </form>
-);
-}
+    <div>
+      <h1>Acceder a tu cuenta</h1>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <div>
+              <label htmlFor="email">Email</label>
+              <Field type="email" name="email" />
+              <ErrorMessage name="email" component="div" />
+            </div>
+            <div>
+              <label htmlFor="password">Contraseña</label>
+              <Field type="password" name="password" />
+              <ErrorMessage name="password" component="div" />
+            </div>
+            <button type="submit" disabled={isSubmitting}>
+              Ingresar
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default Login;
