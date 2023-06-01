9a4aa1d0e9fc42212e59c9f2cf7488332bfeb669
Juan Gomez <juancarlos.gm931@gmail.com>
2023-06-01 17:06:27 -0500

    client/src/components/index.js  agregar
    Se agrega FormNewProduc al index.js

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,5 +3,6 @@ import LandingPage from './LandingPage'
 import Login from './Login'
 import Detail from './Detail';
 import Home from './Home';
+import FormNewProduc from './FormNewProduc';
 
-export {Nav, LandingPage, Login, Detail, Home};
+export { Nav, LandingPage, Login, Detail, Home, FormNewProduc };
