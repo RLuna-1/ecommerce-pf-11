4d0dc74121ca1c9ea8a20c4fe50219e29da2780b
Juan Gomez <juancarlos.gm931@gmail.com>
2023-06-01 17:09:39 -0500

    client/src/view/App.js   agregar
    Se agrega ruta de FormNewProduc

--- client/src/view/App.js
+++ client/src/view/App.js
@@ -1,19 +1,25 @@
-import {Nav, LandingPage, Login, Home} from '../components/index'
+import {
+	Nav,
+	LandingPage,
+	Login,
+	Home,
+	FormNewProduc,
+} from '../components/index';
 import {Routes, Route, useLocation} from 'react-router-dom';
 import '../css/App.css';
 
 function App() {
   const {pathname} = useLocation();
   return (
-    <>
-      {pathname !== "/login" && <Nav />}
-      <Routes>
-        <Route path='/home' element={<Home />} />
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
-        {/*<Route path="/vender" element={<Form />} />*/}
-      </Routes>
-    </>
+		<>
+			{pathname !== '/login' && <Nav />}
+			<Routes>
+				<Route path='/home' element={<Home />} />
+				<Route path='/' element={<LandingPage />} />
+				<Route path='/login' element={<Login />} />
+				<Route path='/vender' element={<FormNewProduc />} />
+			</Routes>
+		</>
   );
 }
 
