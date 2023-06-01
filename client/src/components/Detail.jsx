6d4b59d1412153f32a127953185b98243518986b
Juan Gomez <juancarlos.gm931@gmail.com>
2023-06-01 18:56:42 -0500

    client/src/components/Detail  agregar
    Se agrega estilo al componente usando tailwind.css

--- client/src/components/Detail.jsx
+++ client/src/components/Detail.jsx
@@ -1,58 +1,82 @@
-/*
-import React, { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import { getRecipeByDetail } from '../redux/actions/actions';
-import styles from '../css/Detail.css';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Detail() {
-	const [isLoading, setIsLoading] = useState(true);
-
-	//se asigna nombres random al destructuring y al state nombreDelEstado
-	const { title, summary, image } = useSelector(
-		(state) => state.nombreDelEstado,
-	);
-
-	const dispatch = useDispatch();
-	const { id: idDetail } = useParams;
-
-	useEffect(() => {
-		dispatch(getRecipeByDetail(idDetail));
-		setIsLoading(false);
-	}, [dispatch, idDetail]);
-	//remueve los caracteres de summary
-	const CharacterRemover = (str) => {
-		if (!str) return false;
-		return str.toString().replace(/(<([^>]+)>)/gi, '');
-	};
-
+	//se agrega para remover culaquier caracter a la descripcion
+	// const removerCaracteres = (str) => {
+	// 	if (!str) return false;
+	// 	return str.toString().replace(/(<([^>]+)>)/gi, '');
+	// };
 	return (
-		<div>
-			<main>
-				{isLoading ? (
-					<div className={styles.loading}></div>
-				) : (
-					<div>
+		<main className='flex justify-center items-start min-h-screen bg-gray-100 py-16'>
+			<div className='max-w-5xl mx-auto bg-white shadow-md rounded-lg overflow-hidden flex'>
+				<div className='w-1/2 overflow-hidden'>
+					<div className='h-full w-full'>
+						<img
+							src='https://codersfree.nyc3.cdn.digitaloceanspaces.com/posts/que-es-javascript-descubre-sus-5-principales-usos.jpg'
+							alt='Imagen del producto'
+							className='object-contain h-full w-full ml-3'
+						/>
+					</div>
+				</div>
+				<div className='w-1/2 p-8'>
+					<article>
 						<header>
-							<h1>{title}</h1>
+							<h1 className='text-2xl font-bold mb-4'>
+								Detalles
+							</h1>
 						</header>
-						<div>
-							<img src={image} alt={title} />
-						</div>
-						<div>
-							<p>{CharacterRemover(summary)}</p>
-						</div>
+						<section className='mb-4'>
+							<h2 className='text-xl font-semibold mb-2'>
+								Nombre del producto
+							</h2>
+						</section>
+						<section className='mb-4'>
+							<h2 className='text-xl font-semibold mb-2'>
+								Precio
+							</h2>
+							<p>$ 12</p>
+						</section>
+						<section className='mb-4'>
+							<h2 className='text-xl font-semibold mb-2'>
+								Descripción
+							</h2>
+							<p className='mb-4'>
+								Lorem ipsum dolor sit amet consectetur
+								adipisicing elit. Dolores debitis vitae nobis
+								ducimus laboriosam reiciendis amet aliquid
+								accusantium in dolore! Repellat perferendis
+								voluptatibus quis officia veritatis est
+								explicabo debitis doloribus? Ad inventore in
+								impedit provident. Eum porro delectus odit
+								officia minima asperiores soluta qui numquam
+								possimus, sint aliquam repudiandae molestiae at?
+								Dolore voluptate ipsam recusandae repellat ab,
+								cumque distinctio debitis. Neque dolore nam
+								perspiciatis amet sit
+							</p>
+						</section>
+						<section className='mb-4'>
+							<h2 className='text-xl font-semibold mb-2'>
+								Categoría
+							</h2>
+							<p>nombre de la categoría</p>
+						</section>
 						<section>
 							<Link to={'/home'}>
-								<button>
-									<span>Back to home</span>
+								<button className='bg-[#6F47EB] hover:bg-[#4c1d95] text-white font-bold py-2 px-4 rounded mt-2'>
+									<span>Regresar a inicio</span>
 								</button>
 							</Link>
 						</section>
-					</div>
-				)}
-			</main>
-		</div>
+						<Link to={'/carrito'}>
+							<button className='bg-[#6F47EB] hover:bg-[#4c1d95] text-white font-bold py-2 px-4 rounded mt-2'>
+								<span>Agregar al Carrito</span>
+							</button>
+						</Link>
+					</article>
+				</div>
+			</div>
+		</main>
 	);
 }
-*/
