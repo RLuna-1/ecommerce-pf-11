c5e281d0fb8b15d3dae815abfec0ac83b116a50c
lucas-357 <lucas.rojo.180@gmail.com>
2023-06-02 15:15:09 -0300

    carrito

--- client/src/components/Carrito.jsx
+++ client/src/components/Carrito.jsx
@@ -1,144 +1,145 @@
-import React from "react";
-import AddToBag from "../AddToBag";
-import { Link } from "react-router-dom";
-import Rating from "@mui/material/Rating";
-import { withStyles } from "@material-ui/core/styles";
-import { styled } from "@mui/material/styles";
-import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
-import CardContent from "@mui/material/CardContent";
-import CardActions from "@mui/material/CardActions";
-import Collapse from "@mui/material/Collapse";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import { Button, CardActionArea } from "@mui/material";
 
-import { useSelector } from "react-redux";
+// import React from "react";
+// import AddToBag from "../AddToBag";
+// import { Link } from "react-router-dom";
+// import Rating from "@mui/material/Rating";
+// import { withStyles } from "@material-ui/core/styles";
+// import { styled } from "@mui/material/styles";
+// import Card from "@mui/material/Card";
+// import CardHeader from "@mui/material/CardHeader";
+// import CardMedia from "@mui/material/CardMedia";
+// import CardContent from "@mui/material/CardContent";
+// import CardActions from "@mui/material/CardActions";
+// import Collapse from "@mui/material/Collapse";
+// import IconButton from "@mui/material/IconButton";
+// import Typography from "@mui/material/Typography";
+// import FavoriteIcon from "@mui/icons-material/Favorite";
+// import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+// import { Button, CardActionArea } from "@mui/material";
 
-import favorites from "../redux/favorite.jsx";
-import Stock from "../Stock/Stock";
+// import { useSelector } from "react-redux";
 
-const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
-  marginLeft: "auto",
-  transition: theme.transitions.create("transform", {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
+// import favorites from "../redux/favorite.jsx";
+// import Stock from "../Stock/Stock";
 
-const Carrito = ({
-  image,
-  title,
-  id,
-  price,
-  stock,
-  discount,
-  score,
-  description,
-  bagProducts,
-}) => {
-  const [expanded, setExpanded] = React.useState(false);
-  const user = useSelector((store) => store.userReducer.currentUser);
+// const ExpandMore = styled((props) => {
+//   const { expand, ...other } = props;
+//   return <IconButton {...other} />;
+// })(({ theme, expand }) => ({
+//   transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+//   marginLeft: "auto",
+//   transition: theme.transitions.create("transform", {
+//     duration: theme.transitions.duration.shortest,
+//   }),
+// }));
 
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
+// const Carrito = ({
+//   image,
+//   title,
+//   id,
+//   price,
+//   stock,
+//   discount,
+//   score,
+//   description,
+//   bagProducts,
+// }) => {
+//   const [expanded, setExpanded] = React.useState(false);
+//   const user = useSelector((store) => store.userReducer.currentUser);
 
-  const currentUser = useSelector((state) => state.userReducer.currentUser);
+//   const handleExpandClick = () => {
+//     setExpanded(!expanded);
+//   };
 
-  const addFavorite = () => {
-    if (!currentUser) {
-      return console.log("You must be logged in to add to favorites");
-    }
-    favorites
-      .setFavorite(id, currentUser.userId)
-      .then((res) => {
-        console.log("The product was added to favorites");
-      })
-      .catch((err) => {
-        console.lo(err.message);
-      });
-  };
+//   const currentUser = useSelector((state) => state.userReducer.currentUser);
 
-  return (
-    <>
-      <Card sx={{ maxWidth: 355 }}>
-        {stock <= 0 && <Stock />}
-        <CardActionArea>
-          <CardHeader
-            className="txt_card"
-            title={title}
-            subheader={`NOW: $${(price * ((discount - 1) * -1)).toFixed(
-              2
-            )} BEFORE: $${price}`}
-          />
-          <CardMedia
-            component="img"
-            height="350px"
-            image={image}
-            alt="Main product image"
-          />
-          <CardContent>
-            <Typography variant="body6" color="text.primary">
-              {`STOCK: ${stock}`}
-              <br></br> {`DISCOUNT: ${discount * 100} %`}
-            </Typography>{" "}
-            <Typography variant="body2" color="textSecondary" component="p">
-              {score && <Rating name="read-only" value={score} readOnly />}
-            </Typography>
-          </CardContent>
+//   const addFavorite = () => {
+//     if (!currentUser) {
+//       return console.log("You must be logged in to add to favorites");
+//     }
+//     favorites
+//       .setFavorite(id, currentUser.userId)
+//       .then((res) => {
+//         console.log("The product was added to favorites");
+//       })
+//       .catch((err) => {
+//         console.lo(err.message);
+//       });
+//   };
 
-          <CardActions disableSpacing>
-            {stock > 0 && (
-              <IconButton aria-label="add to favorites">
-                <AddToBag
-                  text={"Añadir al carrito"}
-                  id={id}
-                  user={user}
-                  bagProducts={bagProducts}
-                />
-              </IconButton>
-            )}
-            <Button onClick={addFavorite} className="fav_icon">
-              <FavoriteIcon className="fav_icon" />
-            </Button>
-            <ExpandMore
-              expand={expanded}
-              onClick={handleExpandClick}
-              aria-expanded={expanded}
-              aria-label="show more"
-            >
-              <ExpandMoreIcon />
-            </ExpandMore>
-          </CardActions>
-        </CardActionArea>
-        <Collapse in={expanded} timeout="auto" unmountOnExit>
-          <CardContent>
-            <Typography className="title_info" paragraph>
-              MORE INFO:
-            </Typography>
-            <Typography className="cardInfo" paragraph>
-              {description}
-            </Typography>
-            <div>
-              <Button variant="contained" color="primary">
-                <Link to={`/productDetail/${id}`}>Details</Link>
-              </Button>
-            </div>
-          </CardContent>
-        </Collapse>
-      </Card>
-    </>
-  );
-};
+//   return (
+//     <>
+//       <Card sx={{ maxWidth: 355 }}>
+//         {stock <= 0 && <Stock />}
+//         <CardActionArea>
+//           <CardHeader
+//             className="txt_card"
+//             title={title}
+//             subheader={`NOW: $${(price * ((discount - 1) * -1)).toFixed(
+//               2
+//             )} BEFORE: $${price}`}
+//           />
+//           <CardMedia
+//             component="img"
+//             height="350px"
+//             image={image}
+//             alt="Main product image"
+//           />
+//           <CardContent>
+//             <Typography variant="body6" color="text.primary">
+//               {`STOCK: ${stock}`}
+//               <br></br> {`DISCOUNT: ${discount * 100} %`}
+//             </Typography>{" "}
+//             <Typography variant="body2" color="textSecondary" component="p">
+//               {score && <Rating name="read-only" value={score} readOnly />}
+//             </Typography>
+//           </CardContent>
 
-export default withStyles({
-  title: {},
-  price: {},
-})(Carrito);
+//           <CardActions disableSpacing>
+//             {stock > 0 && (
+//               <IconButton aria-label="add to favorites">
+//                 <AddToBag
+//                   text={"Añadir al carrito"}
+//                   id={id}
+//                   user={user}
+//                   bagProducts={bagProducts}
+//                 />
+//               </IconButton>
+//             )}
+//             <Button onClick={addFavorite} className="fav_icon">
+//               <FavoriteIcon className="fav_icon" />
+//             </Button>
+//             <ExpandMore
+//               expand={expanded}
+//               onClick={handleExpandClick}
+//               aria-expanded={expanded}
+//               aria-label="show more"
+//             >
+//               <ExpandMoreIcon />
+//             </ExpandMore>
+//           </CardActions>
+//         </CardActionArea>
+//         <Collapse in={expanded} timeout="auto" unmountOnExit>
+//           <CardContent>
+//             <Typography className="title_info" paragraph>
+//               MORE INFO:
+//             </Typography>
+//             <Typography className="cardInfo" paragraph>
+//               {description}
+//             </Typography>
+//             <div>
+//               <Button variant="contained" color="primary">
+//                 <Link to={`/productDetail/${id}`}>Details</Link>
+//               </Button>
+//             </div>
+//           </CardContent>
+//         </Collapse>
+//       </Card>
+//     </>
+//   );
+// };
+
+// export default withStyles({
+//   title: {},
+//   price: {},
+// })(Carrito);
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -5,6 +5,7 @@ import Detail from './Detail';
 import Home from '../view/Home';
 import Footer from "./Footer";
 import FormNewProduc from './FormNewProduc';
+import Carrito from './Carrito';
 
 
-export { Nav, LandingPage, Login, Detail, Home, Footer, FormNewProduc };
+export { Nav, LandingPage, Login, Detail, Home, Footer, FormNewProduc, Carrito };
