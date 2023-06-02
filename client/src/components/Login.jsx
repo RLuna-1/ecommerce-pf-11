79d7b6f2e50d104e0b6ff363991d2184f4687f49
lucas-357 <lucas.rojo.180@gmail.com>
2023-06-02 15:10:35 -0300

    login

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
@@ -10,6 +10,14 @@ import Microsoft from "../img/Microsoft.png";
 
 const Login = () => {
   const navigate = useNavigate();
+  const [showCreateAccountForm, setShowCreateAccountForm] = useState(false);
+  const [showLoginForm, setShowLoginForm] = useState(true);
+  const [showDivCuentas, setShowDivCuentas] = useState(true);
+  const [createAccountValues, setCreateAccountValues] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
 
   const initialValues = {
     email: "",
@@ -19,21 +27,37 @@ const Login = () => {
   const validationSchema = Yup.object().shape({
     email: Yup.string()
       .email("Ingrese un email válido")
-      .required("Ingrese su email"),
-    password: Yup.string().required("Ingrese su contraseña"),
+      .required("Ingrese su email")
+      .matches(/@/, "Ingrese un email válido"),
+    password: Yup.string()
+      .required("Ingrese su contraseña")
+      .matches(/^(?=.*[a-zA-Z])(?=.*\d).*$/, "La contraseña debe contener al menos una letra y un número"),
   });
 
-  const handleSubmit = (values, { setSubmitting }) => {
-    // Aquí puedes realizar la lógica de inicio de sesión, como enviar una solicitud al servidor, validar las credenciales, etc.
-    // Verificamos si el usuario y la contraseña coinciden
+  const createAccountValidationSchema = Yup.object().shape({
+    email: Yup.string()
+      .email("Ingrese un email válido")
+      .required("Ingrese su email")
+      .matches(/@/, "Ingrese un email válido"),
+    password: Yup.string()
+      .required("Ingrese su contraseña")
+      .matches(/^(?=.*[a-zA-Z])(?=.*\d).*$/, "La contraseña debe contener al menos una letra y un número"),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref("password"), null], "Las contraseñas deben coincidir")
+      .required("Confirme su contraseña"),
+  });
+
+  const handleSubmit = (values, { setSubmitting, setFieldError }) => {
     if (values.email === "lucas@soyhenry.com" && values.password === "123abc") {
-      // Inicio de sesión exitoso, redireccionar a la ruta "home"
       navigate("/home");
     } else {
-      // Inicio de sesión fallido, puedes mostrar un mensaje de error o realizar alguna otra acción
-      console.log("Inicio de sesión fallido");
+      setFieldError("password", "Inicio de sesión fallido");
     }
     setSubmitting(false);
+  };
+
+  const handleCreateAccountSubmit = (values) => {
+    // Aquí puedes manejar el envío del formulario de creación de cuenta
   };
 
   return (
@@ -52,41 +76,109 @@ const Login = () => {
       </div>
       <div className={style.Login}>
         <h1>Acceder a tu cuenta</h1>
-        <Formik
-          initialValues={initialValues}
-          validationSchema={validationSchema}
-          onSubmit={handleSubmit}
-        >
-          {({ isSubmitting }) => (
-            <Form>
-              <div>
-                <label htmlFor="email">Email: </label>
-                <Field type="email" name="email" />
-                <ErrorMessage name="email" component="div" />
-              </div>
-              <div>
-                <label htmlFor="password">Contraseña: </label>
-                <Field type="password" name="password" />
-                <ErrorMessage name="password" component="div" />
-              </div>
-              <div className={style.DivBotones}>
-                <button
-                  className={style.BotonIniciar}
-                  type="submit"
-                  disabled={isSubmitting}
-                >
-                  Ingresar
-                </button>
-                <button className={style.BotonIniciar}>Crear Cuenta</button>
-              </div>
-              <div className={style.DivCuentas}>
-                <button><img src={Google} className={style.Iconos}/> Continuar con Google</button>
-                <button><img src={Apple} className={style.Iconos}/> Continuar con Apple</button>
-                <button><img src={Microsoft} className={style.Iconos}/> Continuar con Microsoft</button>
-              </div>
-            </Form>
-          )}
-        </Formik>
+        {showLoginForm && (
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}
+          >
+            {({ isSubmitting }) => (
+              <Form>
+                <div>
+                  <label htmlFor="email">Email: </label>
+                  <Field type="email" name="email" />
+                  <ErrorMessage name="email" component="div" />
+                </div>
+                <div>
+                  <label htmlFor="password">Contraseña: </label>
+                  <Field type="password" name="password" />
+                  <ErrorMessage name="password" component="div" />
+                </div>
+                <div className={style.DivBotones}>
+                  <button
+                    className={style.BotonIniciar}
+                    type="submit"
+                    disabled={isSubmitting}
+                  >
+                    Ingresar
+                  </button>
+                  <button
+                    className={style.BotonIniciar}
+                    onClick={() => {
+                      setShowCreateAccountForm(true);
+                      setShowLoginForm(false);
+                      setShowDivCuentas(false); // Ocultar el div DivCuentas
+                    }}
+                  >
+                    Crear Cuenta
+                  </button>
+                </div>
+              </Form>
+            )}
+          </Formik>
+        )}
+        {showCreateAccountForm && (
+          <>
+            <h2>Crear una cuenta</h2>
+            <Formik
+              initialValues={createAccountValues}
+              validationSchema={createAccountValidationSchema}
+              onSubmit={handleCreateAccountSubmit}
+            >
+              {({ isSubmitting }) => (
+                <Form>
+                  <div>
+                    <label htmlFor="email">Email: </label>
+                    <Field type="email" name="email" />
+                    <ErrorMessage name="email" component="div" />
+                  </div>
+                  <div>
+                    <label htmlFor="password">Contraseña: </label>
+                    <Field type="password" name="password" />
+                    <ErrorMessage name="password" component="div" />
+                  </div>
+                  <div>
+                    <label htmlFor="confirmPassword">Confirmar Contraseña: </label>
+                    <Field type="password" name="confirmPassword" />
+                    <ErrorMessage name="confirmPassword" component="div" />
+                  </div>
+                  <div className={style.DivBotones}>
+                    <button
+                      className={style.BotonIniciar}
+                      type="submit"
+                      disabled={isSubmitting}
+                    >
+                      Crear cuenta
+                    </button>
+                    <button
+                      className={style.BotonIniciar}
+                      onClick={() => {
+                        setShowCreateAccountForm(false);
+                        setShowLoginForm(true);
+                        setShowDivCuentas(true); // Ocultar el div DivCuentas
+                      }}
+                    >
+                      Volver
+                    </button>
+                  </div>
+                </Form>
+              )}
+            </Formik>
+          </>
+        )}
+        {showDivCuentas && (
+          <div className={style.DivCuentas}>
+            <button>
+              <img src={Google} className={style.Iconos} /> Continuar con Google
+            </button>
+            <button>
+              <img src={Apple} className={style.Iconos} /> Continuar con Apple
+            </button>
+            <button>
+              <img src={Microsoft} className={style.Iconos} /> Continuar con Microsoft
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
