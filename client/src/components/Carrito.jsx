a2509d81c4f47d67cdcd41f6459f1185af66a2d5
lucas-357 <lucas.rojo.180@gmail.com>
2023-06-02 17:23:21 -0300

    carrito

--- client/src/components/Carrito.jsx
+++ client/src/components/Carrito.jsx
@@ -2,54 +2,145 @@ import React, { useState } from "react";
 import styles from "../css/Carrito.module.css";
 
 const Carrito = () => {
-    const [productos, setProductos] = useState([
-        {
-            id: 1,
-            imagen: require('../img/img01.png'),
-            titulo: 'Full Login+CRUD. VB.NET, MySQL- Nivel Avanzado',
-            precio: 5.99
-        },
-        {
-            id: 2,
-            imagen: require('../img/img02.jpg'),
-            titulo: 'C#- RJ Code Modern UI-M1',
-            precio: 22.50
-        },
-        {
-            id: 3,
-            imagen: require('../img/img03.jpg'),
-            titulo: 'VB.NET- RJ Code Modern UI-M1',
-            precio: 22.50
-        },
-        {
-            id: 4,
-            imagen: require('../img/img04.png'),
-            titulo: 'Full Login+CRUD. C#, SQL Server- Nivel Avanzado',
-            precio: 5.99
-        }
-    ]);
+  const [productos, setProductos] = useState([
+    {
+        id: 1,
+        imagen: require('../img/img01.png'),
+        titulo: 'Full Login+CRUD. VB.NET, MySQL- Nivel Avanzado',
+        precio: 5.99
+    },
+    {
+        id: 2,
+        imagen: require('../img/img02.jpg'),
+        titulo: 'C#- RJ Code Modern UI-M1',
+        precio: 22.50
+    },
+    {
+        id: 3,
+        imagen: require('../img/img03.jpg'),
+        titulo: 'VB.NET- RJ Code Modern UI-M1',
+        precio: 22.50
+    },
+    {
+        id: 4,
+        imagen: require('../img/img04.png'),
+        titulo: 'Full Login+CRUD. C#, SQL Server- Nivel Avanzado',
+        precio: 5.99
+    }
+  ]);
+  const [mostrarFormulario, setMostrarFormulario] = useState(false);
+  const [nombre, setNombre] = useState("");
+  const [dni, setDni] = useState("");
+  const [telefono, setTelefono] = useState("");
+  const [direccion, setDireccion] = useState("");
+  const [codigoPostal, setCodigoPostal] = useState("");
+  const [mensajeCompra, setMensajeCompra] = useState("");
+  const [mostrarBotonComprar, setMostrarBotonComprar] = useState(true);
 
-    const eliminarProducto = (id) => {
-        const nuevosProductos = productos.filter(producto => producto.id !== id);
-        setProductos(nuevosProductos);
-    };
+  const eliminarProducto = (id) => {
+    const nuevosProductos = productos.filter((producto) => producto.id !== id);
+    setProductos(nuevosProductos);
+  };
 
-    return (
-        <div className={styles.General}>
-            {productos.map(producto => (
-                <div key={producto.id} className={styles.Elemento}>
-                    <img src={producto.imagen} alt='Imagen del producto' />
+  const mostrarFormularioEmergente = () => {
+    setMostrarFormulario(true);
+    setMostrarBotonComprar(false); // Oculta el primer botón "Comprar"
+  };
+
+  const ocultarFormularioEmergente = () => {
+    setMostrarFormulario(false);
+    setMostrarBotonComprar(true); // Muestra el primer botón "Comprar" nuevamente
+  };
+
+  const realizarCompra = () => {
+    if (
+      nombre !== "" &&
+      dni !== "" &&
+      telefono !== "" &&
+      direccion !== "" &&
+      codigoPostal !== ""
+    ) {
+      // Realizar lógica de compra aquí (por ejemplo, enviar datos al servidor)
+      setMensajeCompra("¡La compra se ha realizado exitosamente!");
+    } else {
+      setMensajeCompra("Por favor, completa todos los campos del formulario.");
+    }
+  };
+
+  return (
+    <div className={styles.General}>
+      {productos.map((producto) => (
+        <div key={producto.id} className={styles.Elemento}>
+         <img src={producto.imagen} alt='Imagen del producto' />
                     <h1>{producto.titulo}</h1>
                     <h1>$ {producto.precio}</h1>
                     <button className={styles.BotonEliminar} onClick={() => eliminarProducto(producto.id)}>Eliminar</button>
                 </div>
             ))}
-            <button className={styles.Comprar}>Comprar</button>
+
+      {mostrarBotonComprar && (
+        <button
+          className={styles.Comprar}
+          onClick={mostrarFormularioEmergente}
+        >
+          Comprar
+        </button>
+      )}
+
+      {mostrarFormulario && (
+        <div className={styles.Elemento}>
+          <h2>Formulario de Compra</h2>
+          <input
+            type="text"
+            placeholder="Nombre"
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="DNI"
+            value={dni}
+            onChange={(e) => setDni(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Teléfono"
+            value={telefono}
+            onChange={(e) => setTelefono(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Dirección"
+            value={direccion}
+            onChange={(e) => setDireccion(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Código Postal"
+            value={codigoPostal}
+            onChange={(e) => setCodigoPostal(e.target.value)}
+          />
+          <button className={styles.Comprar} onClick={realizarCompra}>
+            Comprar
+          </button>
+          <button className={styles.Comprar} onClick={ocultarFormularioEmergente}>
+            Cancelar
+          </button>
         </div>
-    );
+      )}
+
+      {mensajeCompra && (
+        <div className={styles.MensajeEmergente}>
+          <p>{mensajeCompra}</p>
+          <button onClick={() => setMensajeCompra("")}>Cerrar</button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Carrito;
+
 
 // import React from "react";
 // import AddToBag from "../AddToBag";
