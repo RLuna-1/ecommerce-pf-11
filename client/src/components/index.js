ee55404535ee2844aae8f31c71bcda3b1a1770ff
TriNec13 <ivan.nicolas.valero.11@gmail.com>
2023-06-02 15:19:43 -0300

    App Rutas

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -5,7 +5,7 @@ import Detail from './Detail';
 import Home from '../view/Home';
 import Footer from "./Footer";
 import FormNewProduc from './FormNewProduc';
-import Carrito from './Carrito';
+import Carrito from "./Carrito";
 
 
-export { Nav, LandingPage, Login, Detail, Home, Footer, FormNewProduc, Carrito };
+export { Nav, LandingPage, Login, Detail, Home, Footer, FormNewProduc, Carrito};
