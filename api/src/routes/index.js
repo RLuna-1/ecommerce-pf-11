76d1a4965addfa39dfb9a1f558f690a37b0502da
Tatterate <lucasrissocaballero@gmail.com>
2023-06-11 14:50:42 -0300

    platfoamsfd

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -9,8 +9,8 @@ const paymentsRouter = require("./paymentsRouter");
 const transactionsRouter = require("./transactionsRouter");
 const ratingsRouter = require("./ratingsRouter");
 const authRouter = require("./authRouter");
-const { licenseRouter } = require("./licenseRouter");
-const { platformRouter } = require("./platformRouter");
+// const { licenseRouter } = require("./licenseRouter");
+// const { platformRouter } = require("./platformRouter");
 
 
 const mainRouter = Router();
@@ -20,9 +20,9 @@ mainRouter.use("/products", productsRouter);
 mainRouter.use("/sales", salesRouter);
 mainRouter.use("/wishlist", wishlistRouter);
 mainRouter.use("/saledetails", saledetailsRouter);
-mainRouter.use("/licenses", licenseRouter)
+
 mainRouter.use("/categories", categoriesRouter);
-mainRouter.use("/platforms", platformRouter)
+
 mainRouter.use("/payments", paymentsRouter);
 mainRouter.use("/transactions", transactionsRouter);
 mainRouter.use("/ratings", ratingsRouter);
