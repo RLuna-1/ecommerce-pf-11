f265f6861d5828d13823ed3e3e2dc00d39529423
Iñaki Galindez <98919118+mrmandarina@users.noreply.github.com>
2023-06-20 20:16:27 -0300

    signup con nodemailer

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const { seedDB, seedReviews, seedUsers } = require("./src/utils/index.js");
 
-conn.sync({ alter: true }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(3001, () => {
     seedUsers()
     seedReviews()
